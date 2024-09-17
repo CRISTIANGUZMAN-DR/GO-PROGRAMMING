c80e0791fc4d6c026857e126901897dd62a1ad30
Cristian Daniel Guzman Ramirez <cguzman@almagrario.local>
2024-09-17 16:46:11 -0500

    PAQUETES

---
+++ 003-PAQUETES-GO/gato/hola.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GO-PROGRAMMING/003-PAQUETES-GO/gato"
+)
+
+func main() {
+
+	fmt.Println("Hola desde persona")
+
+	gato.Encontrarproducto()
+	gato.Verprecio()
+}
