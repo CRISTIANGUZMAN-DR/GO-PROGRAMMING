53986e8bba763fa352f065ff8d301e52c883c685
Cristian Guzman <cristianguz75@gmail.com>
2024-09-17 20:08:49 -0500

    Relacion de paquetes en go

--- 003-PAQUETES/persona/main.go
+++ 003-PAQUETES/persona/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"go-programming/003-PAQUETES/producto"
 )
 
 func main() {
+
 	fmt.Println("El vendedor se llama Cristian")
+
+	producto.ConseguirProductos()
+	producto.ObtenerDescuentos()
 
 }
