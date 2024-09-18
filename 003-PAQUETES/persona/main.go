a128bbd3bc7c9bcf3d7cdba921185fea5c3380a0
Cristian Guzman <cristianguz75@gmail.com>
2024-09-17 19:56:54 -0500

    Paquetes

--- 003-PAQUETES/persona/main.go
+++ 003-PAQUETES/persona/main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/GO-PROGRAMMING/003-PAQUETES/producto"
 )
 
 func main() {
 	fmt.Println("El vendedor se llama Cristian")
 
-	producto.ObtenerDescuentos()
-	producto.ConseguirProductos()
 }
